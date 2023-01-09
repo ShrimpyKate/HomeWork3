0e0a1f57916c3505cba673612680774b3e403424
Kate <katekrupnik@gmail.com>
2023-01-10 00:24:57 +0300

    ex1

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int number = 15978;
+int N = 0;
+
+while(number > 0) 
+{
+
+N = N * 10 + number % 10;
+number = number / 10;
+
+}
+ Console.WriteLine(N);
